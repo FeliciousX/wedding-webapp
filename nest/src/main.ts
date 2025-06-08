6793eded26535ee9e512d92b39778e18bfdd9841
FeliciousX <hello@feliciousx.dev>
2025-06-08 23:43:50 +1000

    feat: initial nestjs app with handlebar template

---
+++ nest/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(join(__dirname, '../', 'views'));
+
+  app.use(cookieParser());
+
+  const port = process.env.PORT ?? 3000
+  await app.listen(port);
+
+  console.log(`[INFO] Server is running on port ${port}`);
+}
+bootstrap();
