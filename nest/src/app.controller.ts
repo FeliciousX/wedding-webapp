36746fc63a439b2d76fe5656b3e2e8d89b1e52b3
FeliciousX <hello@feliciousx.dev>
2025-06-11 00:12:54 +1000

    feat: use exponent to create a sample wedding site

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -7,7 +7,21 @@ export class AppController {
 
   @Get()
   @Render('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      title: `Ena & Churchill's wedding - rsvp`,
+      siteName: 'Ena & Churchill\'s Wedding',
+      weddingDate: 'Saturday, November 22nd, 2025',
+      weddingTime: '4:00 PM - 11:00 PM',
+      venueName: 'Cove 55',
+      venueAddress: '93010 Kuching, Sarawak, Malaysia',
+      hotelName: 'Cove 55',
+      hotelAddress: 'Cove 55, 93010 Kuching, Sarawak, Malaysia',
+      coupleLastName: 'Lee',
+      registryStore1: 'Williams Sonoma Registry',
+      registryStore2: 'Target Registry',
+      contactEmail: 'wedding@feliciousx.dev',
+      currentYear: new Date().getFullYear()
+    };
   }
 }
